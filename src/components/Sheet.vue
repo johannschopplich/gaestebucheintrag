b1944eefd57bcca6668237c4c5cccf1a0563441e
Johann Schopplich <mail@johannschopplich.com>
2022-05-22 22:16:07 +0200

    chore: content updates

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -1,14 +1,10 @@
-<script setup lang="ts">
-const color = "rgb(240, 138, 75)";
-</script>
-
 <template>
   <div
     class="h-full text-black grid grid-flow-row auto-rows-fr un-divide border-4 border-black dark:text-white dark:border-white"
   >
     <div class="flex row-span-2">
       <div class="grid grid-rows-[1fr_2fr] un-divide">
-        <div class="heading p-3">Wie heißt du?</div>
+        <div class="heading p-3">Wer bist du und welcher Tag ist heute?</div>
         <div class="heading p-3">
           Welchen Jugendtraum wolltest du immer verwirklichen oder hast du
           vielleicht auch verwirklicht?
@@ -39,20 +35,24 @@ const color = "rgb(240, 138, 75)";
     </div>
 
     <div class="relative">
-      <div class="heading p-3">
-        Drei Werte zwischen 0 und 255 (für Rot, Grün, Blau).
-      </div>
+      <div class="heading p-3">Notiere drei Zahlen zwischen 0 und 255.</div>
       <div class="absolute bottom-3 right-3 flex gap-2 items-center">
-        <div class="w-5 h-5" :style="{ backgroundColor: color }"></div>
         <code class="bg-zinc-100 text-xs rounded px-2 py-1 dark:bg-zinc-800">
-          {{ color }}
+          Rot 240 + Grün 138 + Blau 75 =
         </code>
+        <div
+          class="w-5 h-5"
+          :style="{ backgroundColor: 'rgb(240, 138, 75)' }"
+        />
       </div>
     </div>
 
     <div class="flex">
       <div class="w-2 border-r-4 border-black dark:border-white"></div>
-      <div class="heading p-3">Was möchtest du mir auf den Weg geben?</div>
+      <div class="w-full p-3 flex flex-col justify-between">
+        <div class="heading">Was möchtest du mir auf den Weg geben?</div>
+        <div class="subheading text-right">Deine Heidi</div>
+      </div>
     </div>
   </div>
 </template>
